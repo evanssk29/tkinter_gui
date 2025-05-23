fd394059c3778b4df4782fb2a42dd28e611c621f
evans <evanssk29@gmail.com>
2025-05-22 23:22:33 -0400

    changes to radio_button.py

--- gui.py
+++ gui.py
@@ -6,13 +6,13 @@ def submit(): # function for submit button
     print("Hello", user_name)
     # entry.config(state=DISABLED) # disables entry box
 
-# def click(): # function to track clicks
-#     global count
-#     count += 1
-#     if count == 1:
-#         print("You voted for Pope Juice!!!!!")
-#     else:
-#         print("You voted for Pope Juice", count, "times!!")
+def click(): # function to track clicks
+    global count
+    count += 1
+    if count == 1:
+        print("You voted for Pope Juice!!!!!")
+    else:
+        print("You voted for Pope Juice", count, "times!!")
 
 def delete(): # function to delete all
     entry.delete(0,END)
@@ -20,11 +20,11 @@ def delete(): # function to delete all
 def backspace(): # function to delete one character
     entry.delete(len(entry.get())-1,END)
 
-# def display():
-#     if x.get()==1:
-#         print("You will go London")
-#     else:
-#         print("You are a POS")
+def display():
+    if x.get()==1:
+        print("You will go London")
+    else:
+        print("You are a POS")
 
 window = Tk()  # instantiates an instance of a window
 window.geometry("1000x500")
@@ -49,22 +49,22 @@ entry.pack(side=BOTTOM)
 submit_button = Button(window,text='submit',command=submit)
 submit_button.pack(side=BOTTOM)
 
-# check_button = Checkbutton(window,
-#                            text="Will U go London?",
-#                            variable=x,
-#                            onvalue=1,
-#                            offvalue=0,
-#                            command=display,
-#                            font=('Arial',20),
-#                            fg='red',
-#                            bg='black',
-#                            activeforeground='yellow',
-#                            activebackground='red',
-#                            padx=25,
-#                            pady=10,
-#                            image=go_london,
-#                            compound='left')
-# check_button.pack(side=LEFT)
+check_button = Checkbutton(window,
+                           text="Will U go London?",
+                           variable=x,
+                           onvalue=1,
+                           offvalue=0,
+                           command=display,
+                           font=('Arial',20),
+                           fg='red',
+                           bg='black',
+                           activeforeground='yellow',
+                           activebackground='red',
+                           padx=25,
+                           pady=10,
+                           image=go_london,
+                           compound='left')
+check_button.pack(side=LEFT)
 
 delete_button = Button(window,text='delete',command=delete)
 delete_button.pack(side=BOTTOM)
@@ -77,32 +77,32 @@ window.iconphoto(True, icon)
 
 window.config(background="black")
 
-label = Label(window,
-              text="THAT AINT' GOT NOTHING TO DO WITH ME PAL",
-              font=('Arial', 20, 'bold'),
-              fg='green', bg='black',
-              relief=RAISED,
-              bd=10,
-              padx=20,
-              pady=20,
-              image=photo,
-              compound="bottom")
-label.pack()
+# label = Label(window,
+#               text="THAT AINT' GOT NOTHING TO DO WITH ME PAL",
+#               font=('Arial', 20, 'bold'),
+#               fg='green', bg='black',
+#               relief=RAISED,
+#               bd=10,
+#               padx=20,
+#               pady=20,
+#               image=photo,
+#               compound="bottom")
+# label.pack()
 # label.place(x=100, y=100) # places label at certain cords
 
 
-# button = Button(window,
-#                 text="Click to vote for Pope Juice",
-#                 command=click,
-#                 font=("Comic Sans",15, 'bold'),
-#                 fg='black', bg='red',
-#                 activeforeground='red',
-#                 activebackground='yellow',
-#                 state=ACTIVE,
-#                 image=photo,
-#                 compound='left')
-#
-# count = 0
-# button.pack()
+button = Button(window,
+                text="Click to vote for Pope Juice",
+                command=click,
+                font=("Comic Sans",15, 'bold'),
+                fg='black', bg='red',
+                activeforeground='red',
+                activebackground='yellow',
+                state=ACTIVE,
+                image=photo,
+                compound='left')
+
+count = 0
+button.pack()
 
 window.mainloop()
---
+++ radio_button.py
@@ -0,0 +1,62 @@
+# radio button = similar to checkbox, but you can only select one from a group
+from tkinter import *
+
+senate = ["Pope Juice","Go London","Drew Natural"]
+
+def votes():
+    if(x.get())==0:
+        print("You voted for Pope Juice!")
+    elif(x.get()==1):
+        print("You voted Go London Cat!")
+    elif(x.get()==2):
+        print("You voted for Drew Natural!")
+    else:
+        print("FUCK YOURSELF!")
+
+window = Tk()
+window.config(background="black")
+window.geometry("600x700")
+window.title("******CAST UR VOTE******")
+
+label = Label(window,
+              text="CAST UR VOTE!!",
+              font=('Arial', 15, 'bold'),
+              fg='red', bg='black',
+              relief=RAISED,
+              bd=10,
+              padx=20,
+              pady=20,
+              # compound="bottom"
+              )
+label.pack(anchor='w')
+
+vote_pope = PhotoImage(file='beetlejuice1.png')
+vote_london = PhotoImage(file='PNGgoLondon.png')
+vote_drew = PhotoImage(file='drew_natural.png')
+window.iconphoto(True, vote_drew)
+senante_list = [vote_pope,vote_london, vote_drew]
+
+
+
+
+
+x = IntVar()
+
+for index in range(len(senate)):
+    radiobutton = Radiobutton(window,
+                              text=senate[index], # adds text to radio buttons
+                              variable=x, # groups radiobuttons together if they share the same variable
+                              value=index, # assigns each radiobutton a different value
+                              padx=40,
+                              font=("Impact",30),
+                              fg="red",
+                              bg="black",
+                              image=senante_list[index], # adds image to radiobutton
+                              compound='left', # adds image and text to left side
+                              indicatoron=0,
+                              width=375, # sets width of radiobuttons
+                              command=votes) # sets command of radiobutton to function
+
+    radiobutton.pack(anchor="w")
+
+window.mainloop()
